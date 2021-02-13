ee936907ae7e93fb19ab75e41e73cbe0ec7dbf2b
jbeard <player69>
2021-02-12 21:02:35 -0700

    Adjusted the printProfileData function

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
-
-// Notice the lack of parentheses around the `profileDataArr` parameter?
 const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
 
-    // This is.............
-    for(let i = 0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    };
+  console.log('================');
 
-    console.log("=========");
-
-    // The same as this.............
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem);
-    });
-
-    // Cleaner version of forEach
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
